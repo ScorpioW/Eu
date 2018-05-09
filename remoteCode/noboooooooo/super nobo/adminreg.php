a054ab6643ef4b9fbde50b16d511e1e5cb1a48ce
ibaixot <37900412+ibaixot@users.noreply.github.com>
2018-05-09 10:24:17 +0100

    Add files via upload

--- remoteCode/noboooooooo/super nobo/adminreg.php
+++ remoteCode/noboooooooo/super nobo/adminreg.php
@@ -21,9 +21,32 @@ include("validar.php");
         Password 2 :<input type="text" name="pass2"><br>
         Id do seu contacto :<input type="text" name="idcont"><br>
         Direitos :<input type="text" name="direitos"><br>
+        Localidade :
+        <select name="localidade">
+          <option value="Acores">Açores</option>
+          <option value="Aveiro">Aveiro</option>
+          <option value="Beja">Beja</option>
+          <option value="Castelo Branco">Castelo Branco</option>
+          <option value="Coimbra">Coimbra</option>
+          <option value="Évora">Évora</option>
+          <option value="Faro">Faro</option>
+          <option value="Leiria">Leiria</option>
+          <option value="Lisboa">Lisboa</option>
+          <option value="Madeira">Madeira</option>
+          <option value="Portalegre">Portalegre</option>
+          <option value="Porto">Porto</option>
+          <option value="Santarém">Santarém</option>
+          <option value="Setubal">Setubal</option>
+          <option value="Portalegre">Viana do Castelo</option>
+          <option value="Portalegre">Vila Real</option>
+          <option value="Viseu">Viseu</option>
+        </select>
         <input type="submit" value="Registar">
         <input type="reset" value="Limpar">
         <?php include("reguser.php"); ?>
     </form>
 </body>
 </html>
+
+
+
