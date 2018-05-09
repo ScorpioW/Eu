6b5619434e6eba9af1fd0d687fce20970c59c988
ibaixot <37900412+ibaixot@users.noreply.github.com>
2018-05-09 18:26:23 +0100

    magic

---
+++ remoteCodeV2/contactos/regcontacto.php
@@ -0,0 +1,35 @@
+<?php  
+    header('Content-Type: text/html; charset=UTF-8');
+    include("php/DB.php");
+    include("php/validar.php");
+?>
+
+<!DOCTYPE HTML>
+<!--
+	Phase Shift by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+<?php include("php/header.php") ;?>		
+<!-- Page -->
+	<div id="page" class="container">
+		<section>
+			<header class="major">
+				<h2>Dados do Contacto</h2>
+				
+			</header>
+    		
+	            
+	            <a href="php/regpessoa.php"><input  value="Registar uma pessoa"></a>
+	            <a href="php/regempresa.php"><input  value="Registar uma empresa"></a>
+
+        	
+		</section>
+	</div>
+<!-- /Page -->
+
+</div>
+	<?php include("php/footer.php") ;?>
+	</body>
+</html>
