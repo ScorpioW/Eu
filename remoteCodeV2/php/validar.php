5eb2a072ac0a4c025ef8f8c494c1d395357f10ce
ibaixot <37900412+ibaixot@users.noreply.github.com>
2018-05-10 09:48:39 +0100

    Update validar.php

--- remoteCodeV2/php/validar.php
+++ remoteCodeV2/php/validar.php
@@ -1,7 +1,7 @@
 <?php  
 	header('Content-Type: text/html; charset=UTF-8');
 	session_start();
-	if (!isset($_SESSION["nome"])) {
+	if (!isset($_SESSION["login"])) {
 		echo "Pagina reservada a utilizar";
 		exit;
 	}
