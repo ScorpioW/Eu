eda046c010c836c5d5af84055d8b9174cbee891e
ibaixot <37900412+ibaixot@users.noreply.github.com>
2018-05-15 15:18:18 +0100

    home Admin atualizado

---
+++ RemoteCodeV4/homeAdmin/homeAdmin.php
@@ -0,0 +1,112 @@
+
+
+    	<head>
+		<title>Contactos</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<meta name="viewport" content="width=device-width, initial-scale=1"> 
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.dropotron.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-wide.css" />
+		</noscript>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Wrapper -->
+			<div class="wrapper style1">
+
+				<!-- Header -->
+					<div id="header" style="height: 70px;" class="skel-panels-fixed">
+						<div id="logo">
+							<h5><a href="index.php">Contacto</a> <span class="tag">ParSis</span></h5>
+						</div>
+						<nav id="nav">
+							<ul>
+								<li><a href="homeAdmin.php">Página Administrador</a></li>
+								<li><a href="home.php">Página Utilizador</a></li>
+								<li class="active"><a href="https://chrome.google.com/webstore/detail/google-voice-by-google/kcnhkahnjcbndmmehfkdnkjomaanaooo">Plugin</a></li>
+								<li><a href="php/logout.php">Logout</a></li>
+							</ul>
+						</nav>
+					</div>
+				<!-- Header -->
+	<!-- Banner -->
+		<div id="banner" class="container">
+			<section>
+				<p><strong>Utilizadores (Admin</strong>)</p>
+				
+			</section>
+		</div>
+
+	<!-- Extra -->
+		<div id="extra">
+			<div class="container">
+				<div class="row2">
+					<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 margcols">
+					<section class="4u" style="width: 60%; margin-left: 20%;"> <a href="pesquisaadmin.php" class="image featured"><img src="images/listar.jpg" alt=""></a>
+						<div class="box">
+							<p><strong>Listagem</strong> </p>
+							<a href="pesquisaadmin.php" class="button">Read More</a> </div>
+					</section>
+					</div>
+					<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 margcols">
+						<section class="4u" style="width: 60%; margin-left: 20%;"> <a href="adminreg.php" class="image featured"><img src="images/novo.jpg"  alt=""></a>
+							<div class="box">
+								<p><strong>Novo User</strong> </p>
+								<a href="adminreg.php" class="button">Read More</a> </div>
+						</section>
+					</div>
+					<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 margcols">
+						<section class="4u" style="width: 60%; margin-left: 20%;"> <a href="criacontacto.php" class="image featured"><img src="images/novo.jpg"  alt=""></a>
+							<div class="box">
+								<p><strong>Criar contacto</strong> </p>
+								<a href="criacontacto.php" class="button">Read More</a> </div>
+						</section>
+					</div>
+				</div>
+				<div class="row2">
+					<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 margcols">
+						<section class="4u" style="width: 60%; margin-left: 20%;"> <a href="editar.php" class="image featured"><img src="images/editar.jpg"  alt=""></a>
+							<div class="box">
+								<p><strong>Editar Contatco</strong> </p>
+								<a href="editacont.php" class="button">Read More</a> </div>
+						</section>
+					</div>
+					<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 margcols">
+						<section class="4u" style="width: 60%; margin-left: 20%;"> <a href="editar.php" class="image featured"><img src="images/editar.jpg"  alt=""></a>
+							<div class="box">
+								<p><strong>Editar Users</strong> </p>
+								<a href="editar.php" class="button">Read More</a> </div>
+						</section>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	<br><br>
+</div>
+	<!-- Copyright -->
+			<!-- Copyright -->
+		<div id="copyrightA">
+			<div class="container"> <span class="copyright">Design: <a href="http://templated.co">TEMPLATED</a>(<a href="http://unsplash.com/cc0">CC0</a>)</span>
+				<ul class="icons">
+					<li><a href="#" class="fa fa-facebook"><span>Facebook</span></a></li>
+					<li><a href="#" class="fa fa-twitter"><span>Twitter</span></a></li>
+					<li><a href="#" class="fa fa-google-plus"><span>Google+</span></a></li>
+				</ul>
+			</div>
+		</div>
+	</body>
+</html>	</body>
+</html>
