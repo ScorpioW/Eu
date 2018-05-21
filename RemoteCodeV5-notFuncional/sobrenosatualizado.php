65d9c99ebffdfeaa2b11c49965d3806d3832c195
ibaixot <37900412+ibaixot@users.noreply.github.com>
2018-05-21 11:27:11 +0100

    sobre nós atualizado
    Com o redireccionamento para os nossos emails

---
+++ RemoteCodeV5-notFuncional/sobrenosatualizado.php
@@ -0,0 +1,29 @@
+    <?php include("php/validar.php");?>
+	<!-- Banner -->
+		
+
+	<!-- Extra -->
+		<div id="extra">
+			<div class="container" id="page">
+                <section>
+				  <h2> Projeto de Contactos </h2>
+				  <p>Este projeto foi desenvolvido no âmbito da FCT. 
+					  Consiste em criar um website e base de dados para que se consiga procurar uma empresa ou individuo 
+					  e obter-se as suas informações de contacto que em conjunto com um plugin será possivel
+					  fazer uma chamada por telefone através deste.</p>		  
+					  <p align="right"><font size="3">
+						Diogo Torres 12ºC ; <a href="mailto:diogosilvatorres98@gmail.com">Contactar Diogo</a> <br>
+						Gonçalo Camacho 12ºC ; <a href="mailto:ibaiescola@hotmail.com">Contactar Gonçalo</a><br><br>
+						- Escola ESMTG <br>
+						- Local Estágio ParSis 
+					  </font></p>
+                </section>
+			</div>
+		</div>
+	<br><br>
+</div>
+	<!-- Copyright -->
+		<?php include("php/footer.php"); ?>
+
+	</body>
+</html>
