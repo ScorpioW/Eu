b7a20cf626bf0174874beffe22781381df438912
ibaixot <37900412+ibaixot@users.noreply.github.com>
2018-07-06 09:35:09 +0100

    edit users

--- RemoteCodeV14/editauser2.php
+++ RemoteCodeV14/editauser2.php
@@ -21,11 +21,16 @@ $registos=mysqli_fetch_array($faz_editar);?>
 			<form action="atualiza?id=<?php echo $id;?>" method="POST" autocomplete="off">
 			Username:<font size="2" color="red">&nbsp;*</font>
 			<input type="text" name="login" value='<?php echo $registos['login'];?>'><br>
-			<input type="radio" name="direitos" <?php echo ($registos['direitos'] =='Admin')? 'checked':'' ?> value="Admin"> Administrador &nbsp; &nbsp; 
-			<input type="radio" name="direitos" <?php echo ($registos['direitos'] =='User')? 'checked':'' ?> value="User"> Utilizador <br><br>
+			<input type="radio" name="direitos" <?php echo ($registos['direitos'] =='inactivo')? 'checked':'' ?> value="Admin"> Inactivo &nbsp; &nbsp; 
+			<input type="radio" name="direitos" <?php echo ($registos['direitos'] =='ler')? 'checked':'' ?> value="User"> Ler <br><br>
+			<input type="radio" name="direitos" <?php echo ($registos['direitos'] =='escrever')? 'checked':'' ?> value="User"> Escrever <br><br>
+			<input type="radio" name="direitos"  <?php echo ($registos['direitos'] =='apagar')? 'checked':'' ?> value="User"> Apagar <br><br>
 			<font size="2" color="red">&nbsp;&nbsp;&nbsp;&nbsp;*Campos de preenchimento obrigatório</font><br>
 			<br><input type="submit" value="Actualizar"> <input type="button" value="Voltar" onclick="window.location.href='editauser'">
 		</section>	
 	</div>
 </div>
 <?php include("php/footer.php"); ?>
+<script>
+	$(function(){priv()});
+</script>
